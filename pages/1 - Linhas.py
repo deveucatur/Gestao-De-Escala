c5fa9903a1a55f129fa3b3ccd5d47c33ff73eac7
deveucatur <processos.eucatur@gmail.com>
2024-05-10 18:34:11 -0400

    Atualizando

--- pages/1 - Linhas.py
+++ pages/1 - Linhas.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import json
-import pandas as pd
-import plotly.express as px
 from util import cabecalho
+from datetime import timedelta, date, datetime
+
 
 st.set_page_config(
     "Linhas",
@@ -11,127 +11,139 @@ st.set_page_config(
 )
 
 cabecalho("Rahyan")
-
 with open("documentos/projeto_linhas_horarios.json", "r", encoding="utf-8") as file:
-    data = json.load(file)
+    linhas_dados = json.load(file)
 
-opcoes = []
-for key, value in data.items():
-    linha = value["LINHA"]
-    opcoes.append((f"{key} - {linha}", key))
 
-linhaSelect = st.multiselect("Linhas", opcoes, format_func=lambda x: x[0], placeholder="Escolha as linhas que deseja visualizar")
+def time_by_min(time):
+    return (time.hour*60)+time.minute
 
-if len(linhaSelect) != 0:
-    tasks = []
-    for linhaChave in linhaSelect:
-        if linhaChave[1] in data:
-            linha_data = data[linhaChave[1]]
-            linha = linha_data['LINHA']
-            for direcao, direcao_data in linha_data['PERCURSO'].items():
-                paradas = list(direcao_data.values())
-                for i in range(len(paradas) - 1):
-                    parada_atual = paradas[i]
-                    prox_parada = paradas[i + 1]
-                    tasks.append({
-                        'Linha': linha,
-                        'Direcao': direcao,
-                        'Parada': parada_atual['SECCIONAMENTO'],
-                        'Hora_Inicio': parada_atual['HORÁRIO SAIDA'],
-                        'Hora_Fim': prox_parada['HORÁRIO CHEGADA'],
-                        'Intervalo_Inicio': parada_atual['HORÁRIO CHEGADA'],
-                        'Intervalo_fim': parada_atual['HORÁRIO SAIDA']
-                    })
 
-    df = pd.DataFrame(tasks)
+def diferenca_minutos_entre_times(objeto1, objeto2):
+    minutos_objeto1 = time_by_min(objeto1)
+    minutos_objeto2 = time_by_min(objeto2)
 
-    df['Hora_Inicio'] = pd.to_datetime(df['Hora_Inicio'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
-    df['Hora_Fim'] = pd.to_datetime(df['Hora_Fim'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+    diferenca_minutos = minutos_objeto2 - minutos_objeto1
 
-    df['Duração'] = df['Hora_Fim'] - df['Hora_Inicio']
+    if diferenca_minutos < 0:
+        diferenca_minutos += 24 * 60  
 
-    df = df.dropna(subset=['Hora_Inicio', 'Hora_Fim'])
+    return diferenca_minutos
 
-    if not df.empty:
-        df['Intervalo'] = df['Hora_Fim'] - df['Hora_Inicio']
 
-        df['Intervalo_Segundos'] = df['Intervalo'].dt.total_seconds()
+def seccionamento_by_lin(lin, list_datas):
+    dates_seccionament = []
+    for dat in list_datas:
+        if dat.weekday() in linhas_dados[lin]['FREQUÊNCIA']:
+            dates_seccionament.append(dat)
 
-        fig = px.timeline(df, x_start='Hora_Inicio', x_end='Hora_Fim', y='Linha', color='Direcao', title='Gráfico Gantt de Linhas de Ônibus', width=1250, labels={'Hora_Inicio': 'Horário de Saída', 'Hora_Fim': 'Horário de Chegada'})
+    return dates_seccionament
 
-        st.plotly_chart(fig)
-    else:
-        st.warning("Nenhuma linha selecionada. Por favor, escolha as linhas que deseja visualizar.")
 
-def semana_by_number(string_sem):
-    string_sem = str(string_sem).strip()
-    aux = {'0': "Seg",
-           '1': "Ter",
-           '2': "Qua",
-           '3': "Qui",
-           '4': "Sex",
-           '5': "Sáb",
-           '6': "Dom"}
+def encontra_caracter(texto, caracter):
+    aux_caracter = [x for x in range(len(texto)) if texto[x] == str(caracter).strip()]
+
+    return(aux_caracter)
+
+
+def trata_lin(linha, dados, list_datas):
+    secc_by_lin = seccionamento_by_lin(linha, list_datas)
+
+    retorno = {}
+    for dat_secc in secc_by_lin:
+
+        for direc, value_aux in dados[linha]['PERCURSO'].items():
+        
+            print('-'*30+direc+'-'*30)
+            new_dat_inic = datetime.strptime(f"{dat_secc} {[values['HORÁRIO CHEGADA'][-8:] for key, values in value_aux.items()][0]}", '%Y-%m-%d %H:%M:%S')
+            cont = time_by_min(new_dat_inic.time())
+
+            date_aux = new_dat_inic.date()
+            last_date = new_dat_inic.time()
+
+            for cod_secc, value in value_aux.items():
+                
+                print('-'*10+dados[linha]['PERCURSO'][direc][cod_secc]['SECCIONAMENTO']+'-'*10)
+
+                aux_scc = str(dados[linha]['PERCURSO'][direc][cod_secc]['HORÁRIO CHEGADA']).strip()
+                data_secc = datetime.strptime(str(aux_scc[min(encontra_caracter(aux_scc, ':'))-2:]).strip(), "%H:%M:%S").time()
+
+                dif_min = diferenca_minutos_entre_times(last_date, data_secc)
+
+                last_date = data_secc
+                #SEPARANDO AS HORAS E MINUTOS DA DIFERENÇA
+                cont += dif_min
+                
+                if cont >= 1440:
+                    date_aux += timedelta(days=1)
+                    cont -= 1440
+                
+                #SUBESCREVENDO AS DATAS E HORAS COM O FORMATO CORRETO
+                retorno_hrs_chegada = f'{date_aux} {data_secc.hour}:{data_secc.minute}:00'
+
+                dados[linha]['PERCURSO'][direc][cod_secc]['HORÁRIO CHEGADA'] = retorno_hrs_chegada
+
+                #TRANTANDO O DADO DE TEMPO PARADO PARA SOMAR JUNTO A HORAS CHEGADA
+                parada_time = datetime.strptime(dados[linha]['PERCURSO'][direc][cod_secc]['TEMPO DE PARADA'].strip()[-8:], '%H:%M:%S')
+                parada_timedelta = timedelta(hours=parada_time.hour, minutes=parada_time.minute, seconds=parada_time.second)
+
+                dados[linha]['PERCURSO'][direc][cod_secc]['HORÁRIO SAIDA'] = str(datetime.strptime(retorno_hrs_chegada, "%Y-%m-%d %H:%M:%S") + parada_timedelta)
+
+                print(f'HORAS CHEGADA {retorno_hrs_chegada}')
+                print(f'PARADA {parada_time}')
+                print(f'HORA FINAL {datetime.strptime(retorno_hrs_chegada, "%Y-%m-%d %H:%M:%S") + parada_timedelta}')
+
+        retorno[dat_secc] = dados[linha]
     
-    if string_sem in aux.keys():
-        retorno = aux[string_sem]
-    else:
-        retorno = None
-
     return retorno
 
-for linhaChave in linhaSelect:
-    dadoLinha = data[linhaChave[1]]
 
-    st.title(linhaChave[0])
-    col1, col2 = st.columns(2)
-    with col1:
-        st.subheader(f'Horário de Início: {dadoLinha["HORÁRIO INÍCIO"]}')
-    with col2:
-        st.subheader(f'Frequência: {semana_by_number(dadoLinha["FREQUÊNCIA"][0])}')
 
-    col1, col2, col3 = st.columns([5, 2, 5])
-    with col1:
-        st.write("---")
-    with col2:
-        st.subheader("Percurso (Volta)")
-    with col3:
-        st.write("---")
 
-    for subChave, subValor in dadoLinha["PERCURSO"]["Volta"].items():
-        col1, col2, col3, col4, col5 = st.columns(5)
-        with col1:
-            st.text_input("Código", subChave, key=f'VOLTA: Chave - {subChave} | {subValor} | {linhaChave}', disabled=True)
-        with col2:
-            st.text_input("Seccionamento", subValor["SECCIONAMENTO"], key=f'VOLTA: sec - {subChave} | {subValor["SECCIONAMENTO"]} | {linhaChave}', disabled=True)
-        with col3:
-            st.text_input("Horário de Chegada", subValor["HORÁRIO CHEGADA"], key=f'VOLTA: che - {subChave} | {subValor["HORÁRIO CHEGADA"]} | {linhaChave}', disabled=True)
-        with col4:
-            st.text_input("Tempo de Parada", subValor["TEMPO DE PARADA"], key=f'VOLTA: par - {subChave} | {subValor["TEMPO DE PARADA"]} | {linhaChave}', disabled=True)
-        with col5:
-            st.text_input("Horário de Saída", subValor["HORÁRIO SAIDA"], key=f'VOLTA: sai - {subChave} | {subValor["HORÁRIO SAIDA"]} | {linhaChave}', disabled=True)
 
-    col1, col2, col3 = st.columns([5.5, 2, 5.5])
-    with col1:
-        st.write("---")
-    with col2:
-        st.subheader("Percurso (Ida)")
-    with col3:
-        st.write("---")
 
-    if "Ida" in dadoLinha["PERCURSO"]:
-        for subChave, subValor in dadoLinha["PERCURSO"]["Ida"].items():
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col1:
-                st.text_input("Código", subChave, key=f'IDA: Chave - {subChave} | {subValor} | {linhaChave}', disabled=True)
-            with col2:
-                st.text_input("Seccionamento", subValor["SECCIONAMENTO"], key=f'IDA: sec - {subChave} | {subValor["SECCIONAMENTO"]} | {linhaChave}', disabled=True)
-            with col3:
-                st.text_input("Horário de Chegada", subValor["HORÁRIO CHEGADA"], key=f'IDA: che - {subChave} | {subValor["HORÁRIO CHEGADA"]} | {linhaChave}', disabled=True)
-            with col4:
-                st.text_input("Tempo de Parada", subValor["TEMPO DE PARADA"], key=f'IDA: par - {subChave} | {subValor["TEMPO DE PARADA"]} | {linhaChave}', disabled=True)
-            with col5:
-                st.text_input("Horário de Saída", subValor["HORÁRIO SAIDA"], key=f'IDA: sai - {subChave} | {subValor["HORÁRIO SAIDA"]} | {linhaChave}', disabled=True)
-    else:
-        st.info("Não há dados de 'Ida' para esta linha.")
-    st.write("---")
+#APRESENTAÇÃO DO FRONT
+st.subheader('Frequência Semanal')
+
+date_ini = st.date_input('Início', None)
+
+retorno = None
+if date_ini is not None:
+    retorno = date_ini + timedelta(days=14)
+
+
+date_fim = st.date_input('Fim', retorno)
+
+list_datas = []
+while date_ini <= date_fim:
+    list_datas.append(date_ini)
+    date_ini += timedelta(days=1)
+
+
+teste = trata_lin('09319', linhas_dados, list_datas)
+
+st.info(teste)
+linhas_func = {}
+cont = 0
+#PUXANDO SOMENTE AS LINHAS QUE ATENDEM NAQUELE PERÍODO
+for lin in linhas_dados:
+    
+    secc_by_linha = linhas_dados[lin]['FREQUÊNCIA']
+
+    merge_dates = [dat for dat in list_datas if dat.weekday() in secc_by_linha]
+    if len(merge_dates) > 0: 
+        cont+= 1
+        linhas_func[lin] = linhas_dados[lin]
+
+linhaSelect = ''
+if len(linhas_func) > 0:
+    linhaSelect = st.multiselect("Linhas", list(set([f'{lin} // {value["LINHA"]}' for lin, value in linhas_func.items()])), placeholder="Escolha as linhas que deseja visualizar")
+
+if len(linhaSelect) > 0:
+    dados_select = {lin_select: linhas_func[str(str(lin_select).split('//')[0]).strip()] for lin_select in linhaSelect}
+
+    #st.write(dados_select)
+
+    
+
+
