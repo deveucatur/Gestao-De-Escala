50fa70a4e453c2f4427ef27efadab6b9c1d41566
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-06-12 17:55:30 -0400

    Implantação da tabela com os dados dos motoristas

--- pages/2 - Motoristas.py
+++ pages/2 - Motoristas.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-from util import cabEscala
+from util import cabEscala, tabelas, tituloPage
+from conexao import conexaoBD
+from datetime import datetime
 
 st.set_page_config(
     "Motoristas",
@@ -8,8 +10,68 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
+conexao = conexaoBD()
+mycursor = conexao.cursor()
+
 # cabecalho("Rahyan")
 cabEscala()
+
+hoje = datetime.today().date().strftime("%Y-%m-%d")
+sql = f"""SELECT 
+        *,
+        (
+            SELECT 
+                unidade
+            FROM
+                unidades
+            WHERE 
+                id_unidade = motoristas_lista.fgkey_unidade
+        ),
+        (
+            SELECT 
+                nome_cidade
+            FROM
+                cidades_pontos
+            WHERE 
+                id_cidades = motoristas_lista.fgkey_cidade
+        ),
+        (
+            SELECT 
+                nome_funcao
+            FROM
+                funcao_motorista
+            WHERE
+                id_funcao = motoristas_lista.fgkey_funcao
+        ),
+        (
+            SELECT 
+                GROUP_CONCAT(id_viagem SEPARATOR '~/>')
+            FROM 
+                registro_viagens_mot
+            WHERE 
+                motorista_fgkey = motoristas_lista.id_mot 
+                AND data_ini <= '{hoje}' AND data_fim >= '{hoje}'
+        ),
+        (
+            SELECT 
+                GROUP_CONCAT(motivo SEPARATOR '~/>')
+            FROM 
+                motivo_ausencia
+            WHERE 
+                id_motivo IN (SELECT 
+                        motivo_fgkey
+                    FROM 
+                        motoristas_ausencia
+                    WHERE 
+                        motorista_fgkey = motoristas_lista.id_mot
+                        AND data_ini <= '{hoje}' AND data_fim >= '{hoje}')
+        )
+    FROM 
+        motoristas_lista"""
+mycursor.execute(sql)
+dadosMotorista = mycursor.fetchall()
+
+# st.write(dadosMotorista)
 
 colAux, colBt = st.columns([4, 1])
 
@@ -19,23 +81,25 @@ with colBt:
 if novoMot:
     st.switch_page("pages/4 - novoMotorista.py")
 
-st.title("Dados dos Motoristas")
+tituloPage("Dados dos Motoristas")
 
-planil = pd.read_excel('documentos/ProjetoMotoristas.xlsx', sheet_name='listaMotoristas')
+tabelas("Motorista", dadosMotorista)
 
-columns_plan = [column for column in planil]
+# planil = pd.read_excel('documentos/ProjetoMotoristas.xlsx', sheet_name='listaMotoristas')
 
-listMotoristas = []
+# columns_plan = [column for column in planil]
 
-for idx_mot in range(len(planil['ANO'])):
-    aux_list = [planil[colm][idx_mot] for colm in columns_plan]
+# listMotoristas = []
+
+# for idx_mot in range(len(planil['ANO'])):
+#     aux_list = [planil[colm][idx_mot] for colm in columns_plan]
     
-    listMotoristas.append(aux_list)
+#     listMotoristas.append(aux_list)
 
-rotulos = ["ANO", "EMPRESA", "CHAPA", "NOME", "FUNÇÃO", "GRUPO", "CEEMs", "CIDADE", "ÁREA", "DATA ADMISSÃO", "ATIVO"]
+# rotulos = ["ANO", "EMPRESA", "CHAPA", "NOME", "FUNÇÃO", "GRUPO", "CEEMs", "CIDADE", "ÁREA", "DATA ADMISSÃO", "ATIVO"]
 
-# st.write(listMotoristas)
+# # st.write(listMotoristas)
 
-df = pd.DataFrame(listMotoristas, columns=rotulos)
+# df = pd.DataFrame(listMotoristas, columns=rotulos)
 
-st.table(df)
+# st.table(df)
