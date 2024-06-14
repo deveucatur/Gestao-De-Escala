2604a7264a0ecf0fef7d528894a13954db851184
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-06-14 09:43:11 -0400

    Adicionando fragmento na filtragem dos dados da tabela de motorista para diminuir o gap de tempo para coletar os dados a cada modificação no filtro

--- pages/2 - Motoristas.py
+++ pages/2 - Motoristas.py
@@ -71,8 +71,6 @@ sql = f"""SELECT
 mycursor.execute(sql)
 dadosMotorista = mycursor.fetchall()
 
-# st.write(dadosMotorista)
-
 colAux, colBt = st.columns([4, 1])
 
 with colBt:
@@ -83,59 +81,63 @@ if novoMot:
 
 tituloPage("Dados dos Motoristas")
 
-with st.expander("Filtrar"):
-    filNome = st.text_input("Nome", placeholder="Filtrar por nome")
-    if filNome:
-        dadosMotorista = [x for x in dadosMotorista if filNome.upper() in x[1]]
-    st.divider()
+@st.experimental_fragment
+def fragTabela(dadosMotorista):
+    with st.expander("Filtrar"):
+        filNome = st.text_input("Nome", placeholder="Filtrar por nome")
+        if filNome:
+            dadosMotorista = [x for x in dadosMotorista if filNome.upper() in x[1]]
+        st.divider()
 
-    col1, col2 = st.columns(2)
+        col1, col2 = st.columns(2)
 
-    with col1:
-        # unidades = ["TODAS"]
-        unidades = list(set(x[8] for x in dadosMotorista if x[8]))
-        filUnidade = st.multiselect("Unidades", unidades, placeholder="Selecionar unidades")
-        # if "TODAS" not in filUnidade:
-        if filUnidade:
-            dadosMotorista = [x for x in dadosMotorista if x[8] in filUnidade]
+        with col1:
+            # unidades = ["TODAS"]
+            unidades = list(set(x[8] for x in dadosMotorista if x[8]))
+            filUnidade = st.multiselect("Unidades", unidades, placeholder="Selecionar unidades")
+            # if "TODAS" not in filUnidade:
+            if filUnidade:
+                dadosMotorista = [x for x in dadosMotorista if x[8] in filUnidade]
 
-    with col2:
-        # cidades = ["TODAS"]
-        cidades = list(set(x[9] for x in dadosMotorista if x[9]))
-        filCidade = st.multiselect("Cidades de Origem", cidades, placeholder="Selecionar cidades")
-        # if "TODAS" not in filCidade:
-        if filCidade:
-            dadosMotorista = [x for x in dadosMotorista if x[9] in filCidade]
+        with col2:
+            # cidades = ["TODAS"]
+            cidades = list(set(x[9] for x in dadosMotorista if x[9]))
+            filCidade = st.multiselect("Cidades de Origem", cidades, placeholder="Selecionar cidades")
+            # if "TODAS" not in filCidade:
+            if filCidade:
+                dadosMotorista = [x for x in dadosMotorista if x[9] in filCidade]
 
-    col1, col2, col3 = st.columns([2, 2, 1])
-    with col1:
-        # funcoes = ["TODAS"]
-        funcoes = list(set(x[10] for x in dadosMotorista if x[10]))
-        filFuncao = st.multiselect("Funções", funcoes, placeholder="Selecionar funções")
-        # if "TODAS" not in filFuncao:
-        if filFuncao:
-            dadosMotorista = [x for x in dadosMotorista if x[10] in filFuncao]
-    with col2:
-        situacoes = list({"Escalado" for x in dadosMotorista if x[11]})
-        setSituacoes = set()
-        for subLista in (x[12].split("~/>") for x in dadosMotorista if x[12]):
-            setSituacoes.update(subLista)
-        if any(not x[11] and not x[12] for x in dadosMotorista):
-            situacoes.append("Livre")
-        situacoes += list(setSituacoes)
-        filSituacao = st.multiselect("Situações", situacoes, placeholder="Selecionar situações")
-        if filSituacao:
-            dadosMotorista = [x for x in dadosMotorista if (("Escalado" in filSituacao and x[11]) or any(y in x[12].split("~/>") for y in filSituacao if x[12]) or ("Livre" in filSituacao and not x[11] and not x[12]))]
-    with col3:
-        status = st.radio("Status", ["Ativo", "Inativo"], horizontal=True)
-        if status == "Ativo":
-            dadosMotorista = [x for x in dadosMotorista if x[4] == 1]
-        else:
-            dadosMotorista = [x for x in dadosMotorista if x[4] == 0]
-if len(dadosMotorista) != 0:
-    tabelas("Motorista", dadosMotorista)
-else:
-    st.info("Nenhum motorista encontrado")
+        col1, col2, col3 = st.columns([2, 2, 1])
+        with col1:
+            # funcoes = ["TODAS"]
+            funcoes = list(set(x[10] for x in dadosMotorista if x[10]))
+            filFuncao = st.multiselect("Funções", funcoes, placeholder="Selecionar funções")
+            # if "TODAS" not in filFuncao:
+            if filFuncao:
+                dadosMotorista = [x for x in dadosMotorista if x[10] in filFuncao]
+        with col2:
+            situacoes = list({"Escalado" for x in dadosMotorista if x[11]})
+            setSituacoes = set()
+            for subLista in (x[12].split("~/>") for x in dadosMotorista if x[12]):
+                setSituacoes.update(subLista)
+            if any(not x[11] and not x[12] for x in dadosMotorista):
+                situacoes.append("Livre")
+            situacoes += list(setSituacoes)
+            filSituacao = st.multiselect("Situações", situacoes, placeholder="Selecionar situações")
+            if filSituacao:
+                dadosMotorista = [x for x in dadosMotorista if (("Escalado" in filSituacao and x[11]) or any(y in x[12].split("~/>") for y in filSituacao if x[12]) or ("Livre" in filSituacao and not x[11] and not x[12]))]
+        with col3:
+            status = st.radio("Status", ["Ativo", "Inativo"], horizontal=True)
+            if status == "Ativo":
+                dadosMotorista = [x for x in dadosMotorista if x[4] == 1]
+            else:
+                dadosMotorista = [x for x in dadosMotorista if x[4] == 0]
+    if len(dadosMotorista) != 0:
+        tabelas("Motorista", dadosMotorista)
+    else:
+        st.info("Nenhum motorista encontrado")
+
+fragTabela(dadosMotorista)
 
 if len(st.query_params.to_dict()) != 0:
     pageFuncao = st.query_params["funcao"]
